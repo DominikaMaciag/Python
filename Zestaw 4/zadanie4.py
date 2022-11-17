6bd433371c8373df8a4b54c909c1c66fbe60c70b
DominikaMaciag <Dominika.s45@wp.pl>
2022-11-17 22:23:10 +0100

    exercise 4 done

--- Zestaw 4/zadanie4.py
+++ Zestaw 4/zadanie4.py
@@ -3,3 +3,44 @@
 # nazwach, ale różnych argumentach. Z pomocą dekoratorów pojawiają się w język techniki emulujące takie
 # zachowania. W ramach zadania proszę przestudiować materiał na temat singledispatch oraz
 # singledispatchmethod z modułu functools oraz napisać dowolny kod ilustrujący te przypadki 
+
+from functools import singledispatch, singledispatchmethod
+
+@singledispatch
+def multiply(a, b):
+    raise NotImplementedError('Unsupported type')
+
+@multiply.register(int)
+def _(a,b):
+    print(a*b)
+
+@multiply.register(float)
+def _(a,b):
+    print(a*b)
+
+@multiply.register(str)
+def _(a,b):
+    print(a,"*",b)
+
+class Sum:
+    @singledispatchmethod
+    def add(self, a, b):
+        raise NotImplementedError('Unsupported type')
+
+    @add.register(int)
+    def _(self, a, b):
+        print(a+b)
+
+    @add.register(float)
+    def _(self, a, b):
+        print(a+b)
+
+
+if __name__ == '__main__':
+    multiply(2,7)
+    multiply(2.0,7.0)
+    multiply("2","7")
+
+    s = Sum()
+    s.add(2, 2)
+    s.add(2.0, 2.0)
